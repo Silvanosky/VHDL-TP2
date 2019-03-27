4afdd4eb43785ad6b5d966ccc0279d73efc22fce
Silvanosky <charles@villard.pro>
2019-03-27 12:41:36 +0100

    edit: compteur change

--- TP2_Affichage_VGA_DE10-Lite/src/compteur_adresse.vhd
+++ TP2_Affichage_VGA_DE10-Lite/src/compteur_adresse.vhd
@@ -46,4 +46,37 @@ begin
 	
 	addr <= std_logic_vector(to_unsigned(cnt,15));
 end architecture;
+
+architecture RTL2 of compteur_adresse is
+	signal cnt : integer range 0 to 2**15 - 1;
+	signal x : integer range 0 to 1023;
+	signal y : integer range 0 to 1023;
+begin
+
+	process(clk,rst)
+	begin
+		if rst = '1' then
+			cnt <= 0;
+			aclr <= '0';
+			clken <= '0';
+		elsif rising_edge(clk) then
+			clken <= '1';
+			if 0 <= c_h and c_h < 640 then
+				if 0 <= c_v and c_v < 480 then
+					aclr <= '0';
+					x <= c_h / 4;
+					y <= c_v / 4;
+					cnt <= y * 640 + x;
+				else
+					aclr <= '1';
+					cnt <= 0;
+				end if;
+			else
+				aclr <= '1';	
+			end if;
+		end if;
+	end process;
+	
+	addr <= std_logic_vector(to_unsigned(cnt,15));
+end architecture;
 	
