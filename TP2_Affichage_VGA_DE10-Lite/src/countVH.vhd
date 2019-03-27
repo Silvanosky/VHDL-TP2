17606210e094aa730c93dc2fcbf654e57cc2aab0
Silvanosky <charles@villard.pro>
2019-03-27 09:44:36 +0100

    add: countVH

--- TP2_Affichage_VGA_DE10-Lite/src/countVH.vhd
+++ TP2_Affichage_VGA_DE10-Lite/src/countVH.vhd
@@ -14,20 +14,27 @@ END entity countVH;
 ARCHITECTURE RTL OF countVH IS
 	signal  count_v, count_h : integer RANGE 0 TO 1023;
 BEGIN
-  
-  PROCESS(clk,rst)
-  BEGIN  
-    
-    IF rst = '1'  THEN
-      count_v <= 0;
-      count_h <= 0;
-            
- 	 elsif rising_edge(clk)then
-          
-      
-    END IF;
-  END PROCESS;
-  
-  c_v <= count_v;
-  c_h <= count_h;
+	PROCESS(clk,rst)
+	BEGIN  
+
+		IF rst = '1'  THEN
+			count_v <= 0;
+			count_h <= 0;
+
+		elsif rising_edge(clk)then
+			if count_h = 799 then
+				count_h <= 0;
+				if count_v = 524 then
+					count_v <= 0;
+				else 
+					count_v <= count_v + 1;
+				end if;
+			else
+				count_h <= count_h + 1;
+			end if;
+		END IF;
+	END PROCESS;
+
+	c_v <= count_v;
+	c_h <= count_h;
 END RTL;
